1cd3ad46a8409b6c1d2085d0018bf5621f710e22
Sahil Khan <mohdsahil.here@gmail.com>
2025-12-14 22:21:14 +0530

    change in server file and main is changed to server in json

--- src/server.ts
+++ src/server.ts
@@ -11,13 +11,21 @@ connectDB();
 
 const app: Application = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://productr-frontend.vercel.app"
+    ],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 app.use("/api", authRoutes);
 app.use("/api/products", productRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+  console.log(`Server running on: ${PORT}`);
 });
